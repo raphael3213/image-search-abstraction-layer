68710268a3c7be54be12f9f7f21d7e81d6140ab1
Glitch (imagesearcher12214) <none>
2018-05-26 17:01:12 +0000

    🏆🌔 Checkpoint ./server.js:1174538/372

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var express = require('express');
 var app = express();
 var bing = require('node-bing-api')({ accKey: "6fc8cfa19f0b4218b8e57db73c058442" });
 var mongo=require('mongodb').MongoClient
+var url=process.env.MONGOLAB_URI;
+
+
+
+
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
@@ -14,7 +20,18 @@ app.get("/imager/:img*",function(req,res,next)
         {
   var search=req.params.img;
   var off=req.query;
-  mongo.connect
+  mongo.connect(url,function(err,client)
+                {
+    var db1=client.db('hist');
+    var coll=db1.collection('mainer');
+    
+    coll.insert({
+      "searchTerm":search,
+      "date":new Date()
+    },function(err,data){
+      console.log("logged");
+      client.close()
+                });
 });
 
 // listen for requests :)
